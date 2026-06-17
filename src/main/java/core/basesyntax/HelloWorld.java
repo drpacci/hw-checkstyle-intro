3014624837078340915414f463034ad39a4b6411
Denys <werdumbox@gmail.com>
2026-06-18 00:25:27 +0200

    fixed + and name issues

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,8 +5,7 @@ import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-
-    private String newLine;
+    private String line;
     private int variable;
     private int age;
     private int size;
@@ -21,39 +20,27 @@ public class HelloWorld {
 
         for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
-                result = result
-                        + i
-                        + " ";
+                result = result + i + " ";
             }
         }
         return result;
     }
 
     private void sayHello(String hello) {
-        System.out.println(
-                "Hello, Mate! I am happy to see you! "
-                        + "This class looks weird! "
-                        + "Your task is to fix the checkstyle in this file. "
-                        + "Your goal is to run `mvn clean package` "
-                        + "and receive `BUILD SUCCESS` message"
-        );
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! "
+                + "Your task is to fix the checkstyle in this file."
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
     private void initializeVariables() {
-        System.out.println("Hello mates!");
-
+        line = "Hello mates!";
+        System.out.println(line);
         variable = LocalDate.now().getYear();
-        System.out.println("It is "
-                + variable
-                + " year");
-
+        System.out.println("It is " + variable + "year");
         age = 25;
-        System.out.println("I'm "
-                + age);
-
+        System.out.println("I'm " + age);
         size = 42;
-        System.out.println("The size is "
-                + size);
+        System.out.println("The size is " + size);
     }
 }
