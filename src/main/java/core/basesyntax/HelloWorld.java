ef4f9dac977f41057e1ab286d9a26a9b1abb7916
Denys <werdumbox@gmail.com>
2026-06-17 23:49:26 +0200

    redacted style v2 and extra changes

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,19 +1,20 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Cat;
-import core.basesyntax.Model.Dog;
-
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
-public class Hello_World {
+public class HelloWorld {
+
     private String a;
     private int variable;
-    private int age, size;
-
+    private int age;
+    private int size;
 
     public static void main(String[] args) {
         Dog dog = new Dog();
-        Cat cat = new Cat(); }
+        Cat cat = new Cat();
+    }
 
     private String getFromOneToTen() {
         String result = "";
@@ -26,19 +27,24 @@ public class Hello_World {
         return result;
     }
 
-    private void sayHello(String HELLO) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird!"
-                + "Your task is to fix the checkstyle in this file."
-                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+    private void sayHello(String hello) {
+        System.out.println(
+                "Hello, Mate! I am happy to see you! This class looks weird! "
+                        + "Your task is to fix the checkstyle in this file. "
+                        + "Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message"
+        );
+        System.out.println(hello);
     }
 
     private void initializeVariables() {
         System.out.println("Hello mates!");
+
         variable = LocalDate.now().getYear();
-        System.out.println("It is " + variable + "year");
+        System.out.println("It is " + variable + " year");
+
         age = 25;
         System.out.println("I'm " + age);
+
         size = 42;
         System.out.println("The size is " + size);
     }
